1d7b7d750e9c61af4f2d259c69d467dc89570a75
Joonhyeok Kim <ron@bien.ltd>
2024-01-19 19:06:09 +0900

    first commit

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,19 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ron's Test - Notes",
+};
+
+async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("userId undefined");
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+
+  return <div>{JSON.stringify(allNotes)}</div>;
+}
+
+export default NotesPage;
